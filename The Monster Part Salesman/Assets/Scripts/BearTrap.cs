f8d14225a345d0c5dee3737435c360610c6720cf
Josh OConnor <17jtoc@queensu.ca>
2021-05-30 21:50:20 -0400

    bear trap prefab

---
+++ The Monster Part Salesman/Assets/Scripts/BearTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+
+    private Animator animator;
+    public bool ready = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TrapCo());
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator TrapCo()
+    {
+        yield return new WaitForSeconds(0.45f);
+        animator.SetBool("readyTrap", false);
+        ready = true;
+    }
+}
