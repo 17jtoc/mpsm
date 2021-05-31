2458a1977e08cf0f77477dd96df048eaa7bced89
Josh OConnor <17jtoc@queensu.ca>
2021-05-31 16:38:20 -0400

    Trap Placement Tuning

--- The Monster Part Salesman/Assets/Scripts/BearTrap.cs
+++ The Monster Part Salesman/Assets/Scripts/BearTrap.cs
@@ -6,12 +6,17 @@ public class BearTrap : MonoBehaviour
 {
 
     private Animator animator;
+    private Collider2D trapCollider;
     public bool ready = false;
+    public bool debug = false;
     // Start is called before the first frame update
     void Start()
     {
+        
         StartCoroutine(TrapCo());
         animator = GetComponent<Animator>();
+        trapCollider = GetComponent<Collider2D>();
+        trapCollider.enabled = false;
 
     }
 
@@ -24,7 +29,21 @@ public class BearTrap : MonoBehaviour
     private IEnumerator TrapCo()
     {
         yield return new WaitForSeconds(0.45f);
-        animator.SetBool("readyTrap", false);
+        animator.SetBool("readyTrap", true);
         ready = true;
+        trapCollider.enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        debug = true;
+        if (collision.CompareTag("destroytrap"))
+        {
+            
+            Object.Destroy(this);
+
+
+
+        }
     }
 }
