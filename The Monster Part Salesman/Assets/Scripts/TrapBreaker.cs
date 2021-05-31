fa14b03ab250d86efb1d819e16c72a7f9cc485c5
Josh OConnor <17jtoc@queensu.ca>
2021-05-31 18:21:53 -0400

    Revert "changes"
    This reverts commit 39f68c796c19d18daf0448e43e61f7a960e4cb9d.

---
+++ The Monster Part Salesman/Assets/Scripts/TrapBreaker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBreaker : MonoBehaviour
+{
+    public bool debug = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        debug = true;
+        if (collision.CompareTag("trap"))
+        {
+
+            Destroy(collision.gameObject);
+
+
+
+        }
+    }
+}
