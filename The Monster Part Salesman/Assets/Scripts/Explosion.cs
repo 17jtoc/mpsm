03e6691d2d617d83accedb07e73210b8d7b2ab7b
Josh OConnor <17jtoc@queensu.ca>
2021-07-10 11:24:53 -0400

    Explosion Object Added

---
+++ The Monster Part Salesman/Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public int testing = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 0.55f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
